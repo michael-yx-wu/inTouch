2f66b13d175e3ffb1d906f5469194712971d3aac
Naicheng <nathanwhitman1@gmail.com>
2014-03-01 18:58:45 -0500

    Added slider to main.Storyboard

--- inTouch/ViewController.m
+++ inTouch/ViewController.m
@@ -2,18 +2,66 @@
 //  ViewController.m
 //  inTouch
 //
-//  Created by Michael Wu on 2/28/14.
-//  Copyright (c) 2014 Michael Wu. All rights reserved.
+//  Created by Naicheng Wangyu on 03/01/14.
+//  Copyright (c) 2014 Naicheng Wangyu. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
+// Display the name of the current contact
+@property (weak, nonatomic) IBOutlet UILabel *displayName;
+// Display the frequency as user uses the slider
+@property (weak, nonatomic) IBOutlet UILabel *viewFrequency;
+// Display the contact picture
+@property (weak, nonatomic) IBOutlet UIImageView *contactPicture;
+
 @end
 
 @implementation ViewController
 
+// Button for "manually contacted" someone, that's not a swipe action.
+- (IBAction)manuallyContacted:(id)sender {
+}
+
+// Slider to adjust the frequency of desired contact
+- (IBAction)changeFrequency:(id)sender {
+    UISlider *freqSlider = (UISlider *)sender;
+    
+    // Default value or a pre-existing value needs to be determined
+
+    freqSlider.continuous = YES;
+    
+    // Select the value of the slider
+    int SliderValue;
+    
+    // Max option is just to set as 365 days, or annually
+    if (freqSlider.value > 359){
+        SliderValue = (int)365;
+    }
+    // If frequency is greater than quarterly
+    if (freqSlider.value > 120) {
+        SliderValue = 30.0 * floor(((int)roundf(freqSlider.value)/30.0)+0.5);
+    // If greater than monthly
+    }else if (freqSlider.value > 30){
+        SliderValue = 10.0 * floor(((int)roundf(freqSlider.value)/10.0)+0.5);
+    // If less often than monthly
+    }else{
+        SliderValue= (int)roundf(freqSlider.value);
+    }
+
+    
+    // Base case of days selected is 1
+    if(SliderValue == 1){
+        NSString *message = [NSString stringWithFormat:@"Remind me every day"];
+        [self.viewFrequency setText:message];
+    } else{
+    NSString *message = [NSString stringWithFormat:@"Remind every %d days", SliderValue];
+    [self.viewFrequency setText:message];
+    }
+}
+    
 - (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
@@ -23,5 +71,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
