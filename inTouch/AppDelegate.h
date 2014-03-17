5bbc3b96f0def46f3ebf4fc4b106b68066f8b7a4
Michael Wu <michael.yixuan.wu@gmail.com>
2014-03-17 00:09:53 -0400

    Creating nsmanagedobject for each contact.

--- inTouch/AppDelegate.h
+++ inTouch/AppDelegate.h
@@ -6,11 +6,12 @@
 //  Copyright (c) 2014 Naicheng Wangyu. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 
 - (void)updateContacts;
 
--- inTouch/AppDelegate.m
+++ inTouch/AppDelegate.m
@@ -12,6 +12,10 @@
 
 @implementation AppDelegate
 
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize managedObjectContext = _managedObjectContext;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Setting debug level to 1 (everything will be printed)
     [DebugLogger setDebugLevel:1];
@@ -20,13 +24,16 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types
+    // of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application
+    // and it begins the transition to the background state. Use this method to pause ongoing tasks, disable timers, and
+    // throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state
+    // information to restore your application to its current state in case it is terminated later. If your application
+    // supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -69,14 +76,24 @@
     NSArray *allContacts = (__bridge NSArray*)ABAddressBookCopyArrayOfAllPeople(addressBookRef);
     
     // Loop through contacts
+    NSManagedObjectModel *contact;
     for (int i = 0; i < [allContacts count]; i++) {
         ABRecordRef currentContact = (__bridge ABRecordRef)[allContacts objectAtIndex:i];
         
-        // Extract contact information
+        // Get name
         NSString *firstName = (__bridge_transfer NSString*)ABRecordCopyValue(currentContact, kABPersonFirstNameProperty);
         NSString *lastName = (__bridge_transfer NSString*)ABRecordCopyValue(currentContact, kABPersonLastNameProperty);
         [DebugLogger log:[NSString stringWithFormat:@"First Name: %@", firstName] withPriority:1];
         [DebugLogger log:[NSString stringWithFormat:@"Last Name: %@", lastName] withPriority:1];
+        
+        // Get contact photo
+        NSData *contactPhoto;
+        if (ABPersonHasImageData(currentContact)) {
+            contactPhoto = (__bridge_transfer NSData *)ABPersonCopyImageData(currentContact);
+            [DebugLogger log:@"Got contact photo" withPriority:1];
+        } else {
+            [DebugLogger log:@"No contact photo" withPriority:1];
+        }
         
         // Get home, mobile, and work phone numbers
         // May need modify core data later to allow more phone number types
@@ -120,6 +137,21 @@
                 [DebugLogger log:[NSString stringWithFormat:@"Work Email: %@", emailWork] withPriority:1];
             }
         }
+        
+        // Create ManageObject and populate with data
+        // Note: some fields may be set to null
+        contact = [NSEntityDescription
+                   insertNewObjectForEntityForName:@"Contact"
+                   inManagedObjectContext:[self managedObjectContext]];
+        [contact setValue:firstName forKey:@"nameFirst"];
+        [contact setValue:lastName forKey:@"nameLast"];
+        [contact setValue:contactPhoto forKey:@"contactPhoto"];
+        [contact setValue:emailHome forKey:@"emailHome"];
+        [contact setValue:emailOther forKey:@"emailOther"];
+        [contact setValue:emailWork forKey:@"emailWork"];
+        [contact setValue:phoneHome forKey:@"phoneHome"];
+        [contact setValue:phoneMobile forKey:@"phoneMobile"];
+        [contact setValue:phoneWork forKey:@"phoneWork"];
     }
 }
 
