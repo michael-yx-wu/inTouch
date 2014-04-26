0fea76a51269a815a7bb9d1d2ad6bf85c0ad5f1f
Michael Wu <michael.yixuan.wu@gmail.com>
2014-04-26 12:24:26 -0400

    Updating settings controller.

--- inTouch/SettingsViewController.m
+++ inTouch/SettingsViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 inTouch Team. All rights reserved.
 //
 
+#import "ContactManager.h"
 #import "SettingsViewController.h"
 
 @interface SettingsViewController ()
@@ -37,6 +38,26 @@
 - (void)dismissSave:(id)sender {
     // save the settings
     [self dismissViewControllerAnimated:YES completion:NO];
+}
+
+// Display the "syncing contacts" message and sync contacts
+- (IBAction)syncContacts:(id)sender {
+    // Show the busy view
+//    [self disableInteraction];
+//    [DebugLogger log:@"Showing busy view" withPriority:2];
+//    [UIView animateWithDuration:0.15 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+//        [busyView setAlpha:1];
+//        [activityIndicator startAnimating];
+//    } completion:^(BOOL finished) {
+//        [ContactManager updateInformation];
+//        [ContactManager updateUrgency];
+//        [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+//            [busyView setAlpha:0];
+//        } completion:^(BOOL finished){
+//            [activityIndicator stopAnimating];
+//            [self getNextContact];
+//        }];
+//    }];
 }
 
 @end
