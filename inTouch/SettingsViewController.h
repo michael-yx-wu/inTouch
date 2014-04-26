614ded00bfe3541d73ac540482d91f3b032e7d8a
Michael Wu <michael.yixuan.wu@gmail.com>
2014-04-25 22:00:28 -0400

    Adding settings view controller.

---
+++ inTouch/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  inTouch
+//
+//  Created by Michael Wu on 4/25/14.
+//  Copyright (c) 2014 inTouch Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ inTouch/SettingsViewController.m
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.m
+//  inTouch
+//
+//  Created by Michael Wu on 4/25/14.
+//  Copyright (c) 2014 inTouch Team. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
