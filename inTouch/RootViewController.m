e01c46b12441f01181cb11c4742a26ff1c43895e
Michael Wu <michael.yixuan.wu@gmail.com>
2015-07-01 12:13:24 -0400

    Hotfix: disable login screen until we have account-specific content

--- inTouch/RootViewController.m
+++ inTouch/RootViewController.m
@@ -12,27 +12,31 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    // Check if global data entity exists
-    NSManagedObjectContext *moc = [self managedObjectContext];
-    NSManagedObjectModel *model = [self managedObjectModel];
-    NSFetchRequest *request = [model fetchRequestFromTemplateWithName:@"GlobalData" substitutionVariables:NULL];
+    [self performSegueWithIdentifier:@"mainView" sender:self];
     
-    NSError *error;
-    NSArray *results = [moc executeFetchRequest:request error:&error];
-    if (results == nil) {
-        [DebugLogger log:[NSString stringWithFormat:@"Fetch error: %@, %@",
-                          error, [error userInfo]] withPriority:appDelegatePriority];
-        abort();
-    }
-    GlobalData *globalData = [results objectAtIndex:0];
-    if ([globalData accessToken] == nil) {
-        [DebugLogger log:@"Not logged in: showing login view" withPriority:rootViewControllerPriority];
-        [self performSegueWithIdentifier:@"login" sender:self];
-    } else {
-        [DebugLogger log:[NSString stringWithFormat:@"Access token: %@", [globalData accessToken]]
-            withPriority:rootViewControllerPriority];
-        [self performSegueWithIdentifier:@"mainView" sender:self];
-    }
+    /* Hotfix: disable login screen until we have account-specific content  */
+    
+//    // Check if global data entity exists
+//    NSManagedObjectContext *moc = [self managedObjectContext];
+//    NSManagedObjectModel *model = [self managedObjectModel];
+//    NSFetchRequest *request = [model fetchRequestFromTemplateWithName:@"GlobalData" substitutionVariables:NULL];
+//    
+//    NSError *error;
+//    NSArray *results = [moc executeFetchRequest:request error:&error];
+//    if (results == nil) {
+//        [DebugLogger log:[NSString stringWithFormat:@"Fetch error: %@, %@",
+//                          error, [error userInfo]] withPriority:appDelegatePriority];
+//        abort();
+//    }
+//    GlobalData *globalData = [results objectAtIndex:0];
+//    if ([globalData accessToken] == nil) {
+//        [DebugLogger log:@"Not logged in: showing login view" withPriority:rootViewControllerPriority];
+//        [self performSegueWithIdentifier:@"login" sender:self];
+//    } else {
+//        [DebugLogger log:[NSString stringWithFormat:@"Access token: %@", [globalData accessToken]]
+//            withPriority:rootViewControllerPriority];
+//        [self performSegueWithIdentifier:@"mainView" sender:self];
+//    }
 }
 
 #pragma mark - Core Data accessor methods
